cdb708b55fab8ea1d23371d221ac07a636bff0b6
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2021-06-07 18:48:10 +0200

    Change package description
    The package description is changed to include the general purpose of the
    package.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ModelSetup: Utility Supporting Initial Model Setup
+
+ModelSetup is a Python utility that supports an initial setup of a
+computational model that may utilize spiking neural networks implemented using
+Nengo and the robot simulator V-REP.
--- modelsetup/__init__.py
+++ modelsetup/__init__.py
@@ -1,4 +1,9 @@
-"""Utility supporting initial model setup."""
+"""Utility supporting initial model setup.
+
+ModelSetup is a Python utility that supports an initial setup of a
+computational model that may utilize spiking neural networks implemented using
+Nengo and the robot simulator V-REP.
+"""
 
 __version__ = '0.1.0.dev1'
 __author__ = "Przemyslaw (Mack) Nowak"
