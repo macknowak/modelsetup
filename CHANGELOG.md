7801832885552a0f48714d4000a9f0ea0f9996cd
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2021-06-11 20:02:32 +0200

    Add parsing configuration file
    The configuration file is used to specify the desired configuration. Its
    format follows that of the INI file. There are 5 sections in this file:
    
    - 'VirtualEnv' - describes a virtual environment to be created in the
      project directory of the model and supports the following keys:
      'Directory', 'Interpreter', 'SystemSitePackages';
    - 'Dependencies' - describes which required dependencies should be
      installed in the virtual environment and supports the following keys:
      'OverrideRequirementsFile', 'SimTools', 'VRepSim', 'Nengo';
    - 'OptionalDependencies' - describes which optional dependencies should
      be installed in the virtual environment (they are installed
      independently of the required dependencies described in the previous
      section) and supports the following key: 'NengoGui';
    - 'VRep' - describes the robot simulator V-REP used by the model (if
      this is the case) to allow installing its remote API bindings in the
      virtual environment and supports the following key: 'Directory';
    - 'ExtraDirectoriesAndFiles' - describes additional directories and
      files to be created in the project directory of the model and supports
      the following keys: 'CreateSimulationsDir', 'CreateSetupFile'.
    
    Dependencies (both required and optional) can be specified in any of the
    following forms:
    
    - version - used for installing packages from The Python Package Index
      (PyPI);
    - URL - used for installing packages from online repositories;
    - file name (or path to a file) - used for installing packages from
      downloaded archive files;
    - directory name (or path to a directory) - used for installing packages
      in editable/develop mode.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ Changelog
 0.1.0 - Unreleased
 ------------------
 
+### Added
+
+- Added parsing a configuration file (function `parse_config()`). It is a
+  function that reads a configuration file and determines the desired
+  configuration with respect to the virtual environment to be created,
+  dependencies to be installed, and optional additional directories and files
+  to be created.
+
 ### Documentation
 
 - Added package description (file `README.md`).
--- modelsetup/__init__.py
+++ modelsetup/__init__.py
@@ -7,3 +7,44 @@ Nengo and the robot simulator V-REP.
 
 __version__ = '0.1.0.dev1'
 __author__ = "Przemyslaw (Mack) Nowak"
+
+import collections
+import configparser
+
+
+def parse_config(filename):
+    """Parse configuration file."""
+    parser = configparser.ConfigParser()
+    parser.read(filename)
+    cfg = {}
+    venv_dirname = parser.get('VirtualEnv', 'Directory')
+    if venv_dirname:
+        cfg['venv_dirname'] = venv_dirname
+    else:
+        raise ValueError("empty option: 'Directory'")
+    cfg['venv_python_exec'] = parser.get('VirtualEnv', 'Interpreter')
+    cfg['venv_system_site_packages'] = parser.getboolean('VirtualEnv',
+                                                         'SystemSitePackages')
+    cfg['use_requirements_file'] = not parser.getboolean(
+        'Dependencies', 'OverrideRequirementsFile')
+    deps = collections.OrderedDict()
+    if not cfg['use_requirements_file']:
+        simtools_spec = parser.get('Dependencies', 'SimTools')
+        if simtools_spec:
+            deps['simtools'] = simtools_spec
+        vrepsim_spec = parser.get('Dependencies', 'VRepSim')
+        if vrepsim_spec:
+            deps['vrepsim'] = vrepsim_spec
+        nengo_spec = parser.get('Dependencies', 'Nengo')
+        if nengo_spec:
+            deps['nengo'] = nengo_spec
+    nengo_gui_spec = parser.get('OptionalDependencies', 'NengoGui')
+    if nengo_gui_spec:
+        deps['nengo_gui'] = nengo_gui_spec
+    cfg['deps'] = deps
+    cfg['vrep_dirname'] = parser.get('VRep', 'Directory')
+    cfg['create_simulations_dir'] = parser.getboolean(
+        'ExtraDirectoriesAndFiles', 'CreateSimulationsDir')
+    cfg['create_setup_file'] = parser.getboolean('ExtraDirectoriesAndFiles',
+                                                 'CreateSetupFile')
+    return cfg
