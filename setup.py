5a23f93ab4b9da1b32a6787a219c30f9b1084d53
Przemyslaw (Mack) Nowak <pnowak.mack@gmail.com>
2021-07-12 14:30:30 +0200

    Release version 0.1.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read_description(filename):
 
 setup(
     name="modelsetup",
-    version='0.1.0.dev1',
+    version='0.1.0',
     author="Przemyslaw (Mack) Nowak",
     author_email="pnowak.mack@gmail.com",
     description="Utility supporting initial model setup",
